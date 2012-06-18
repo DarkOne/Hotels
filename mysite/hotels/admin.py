51ab9cc9fc2474b1c6de2acaf4813f59e31ce205
Alexey Dolgiy <dolgyi@gmail.com>
2012-06-19 01:44:39 +0300

    Доработана админка

--- mysite/hotels/admin.py
+++ mysite/hotels/admin.py
@@ -6,8 +6,24 @@ class BookingAdmin(admin.ModelAdmin):
     search_fields = ('room',)
     raw_id_fields = ('room',)
 
-admin.site.register(City)
-admin.site.register(Hotel)
-admin.site.register(HotelRoom)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hotel', 'price', 'guests_count')
+    #ordering = ('hotel', 'name')
+    search_fields = ('name', 'hotel__name', 'hotel__city__name')
+    raw_id_fields = ('hotel',)
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'country')
+    search_fields = ('name', 'city__name', 'country__name')
+    #raw_id_fields = ('hotel',)
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+    search_fields = ('name', 'country')
+    ordering = ('name',)
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Hotel, HotelAdmin)
+admin.site.register(HotelRoom, RoomAdmin)
 admin.site.register(Country)
 admin.site.register(Booking, BookingAdmin)
